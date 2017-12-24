094d6d080cf10558672a296b0ff52bcd0c7bcd52
NotLebedev <artem-itf@yandex.ru>
2017-12-24 12:51:14 +0300

    Removed some old code

--- DataProcessor/Dictionary.cpp
+++ DataProcessor/Dictionary.cpp
@@ -33,44 +33,6 @@ Dictionary::Dictionary() {
 
     dictionary[BASE_ADR] = 10;
 
-    /*dictionary[TOP_ADR] = 60;
-
-    //TEMPORARY TODO: remove
-    dictionary[31] = 5;
-    *((char *) (&dictionary[32])) = 'F';
-    *((char *) (&dictionary[33])) = 'O';
-    *((char *) (&dictionary[34])) = 'R';
-    *((char *) (&dictionary[35])) = 'T';
-    *((char *) (&dictionary[36])) = 'H';
-    dictionary[37] = 0;
-    dictionary[38] = -1;
-    dictionary[39] = -1;
-
-    dictionary[40] = 1;
-    *((char *) (&dictionary[41])) = ':';
-    dictionary[42] = 1;
-    dictionary[43] = 31;
-    dictionary[44] = 0;
-    dictionary[45] = 1;
-    dictionary[46] = 0;
-    dictionary[47] = STATE_ADR;
-    dictionary[48] = 10;
-    dictionary[49] = -1;
-
-    dictionary[50] = 1;
-    *((char *) (&dictionary[51])) = ';';
-    dictionary[52] = 1;
-    dictionary[53] = 40;
-    dictionary[54] = 0;
-    dictionary[55] = 0;
-    dictionary[56] = 0;
-    dictionary[57] = STATE_ADR;
-    dictionary[58] = 10;
-    dictionary[59] = -1;
-
-    dictionary[CONTEXT_ADR] = 50;*/
-
-
 }
 
 Dictionary::~Dictionary() {
