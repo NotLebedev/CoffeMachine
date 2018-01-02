c4ff6d5b78901d79c959d810632c855777bdf498
NotLebedev <artem-itf@yandex.ru>
2018-01-02 20:53:43 +0300

    Implemented getStack() nad getDicitionary()

--- ModuleSupport/UniversalModuleInterface.cpp
+++ ModuleSupport/UniversalModuleInterface.cpp
@@ -28,9 +28,23 @@ ERROR_TYPE UniversalModuleInterface::executeCommand(std::string& command) {
 }
 
 ERROR_TYPE UniversalModuleInterface::getStack(iWORD *data) {
+
+    executionProcessor->stackOut(data);
+
     return 0;
 }
 
 ERROR_TYPE UniversalModuleInterface::getDictionary(iWORD *data, size_t startAdr, size_t endAdr) {
+
+    data = new iWORD[endAdr - startAdr + 1];
+
+    for (int i = (int)(startAdr); i < endAdr + 1; ++i) {
+
+        executionProcessor->stackIN(&i);
+        executionProcessor->fetch();
+        executionProcessor->stackOut(&data[i - startAdr]);
+
+    }
+
     return 0;
 }
